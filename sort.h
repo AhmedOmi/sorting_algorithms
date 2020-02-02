10f90c459db67d6f38ded67bb714eb60aaff164b
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-02 21:21:02 +0000

    bubble sort function

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,11 +9,10 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
