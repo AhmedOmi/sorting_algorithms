bc2ef2a77157881718d457beb6709c102c157a3e
inoublii <inoublii@gmail.com>
2020-02-05 21:35:56 +0100

    fixed

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,21 +9,19 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	long int jmp = 1, i, j, tmp;
-
-	while (jmp < ((int)(size)))
-		jmp = (3 * jmp) + 1;
-	for (jmp = (jmp - 1) / 3; jmp > 0; jmp = (jmp - 1) / 3)
-	{
-	for (i = jmp; i < (int)size; i += 1)
-		{
-		tmp = array[i];
-
-	for (j = i; j >= jmp &&
-	array[j - jmp] > tmp; j -= jmp)
-	array[j] = array[j - jmp];
-	array[j] = tmp;
-		}
-	print_array(array, size);
-	}
+long int jmp = 1, i, j, tmp;
+while (jmp < ((int)(size)))
+jmp = (3 * jmp) +1;
+for (jmp = (jmp - 1) / 3; jmp > 0; jmp = (jmp - 1) / 3)
+{
+for (i = jmp; i < (int)size; i += 1)
+{
+tmp = array[i];
+for (j = i; j >= jmp &&
+array[j - jmp] > tmp; j -= jmp)
+array[j] = array[j - jmp];
+array[j] = tmp;
+}
+print_array(array, size);
+}
 }
