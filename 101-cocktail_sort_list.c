31047c463ed63800ede9ad741c899b3777d3ae31
inoublii <inoublii@gmail.com>
2020-02-05 22:07:19 +0100

    cocktail

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - Cocktail sort
+ * @list: doubly linked list
+ */
+
+void cocktail_sort_list(listint_t **list)
