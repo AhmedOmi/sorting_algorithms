e87bf1d235535cc648db866cbaa23f7142212c6b
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-05 15:59:43 +0000

    update sort insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ listint_t *New_list;
 if (list == NULL)
 return;
 New_list = (*list)->next;
-while (New_list)
+while (New_list != NULL)
 {
 while (New_list->prev != NULL && New_list->prev->n > New_list->n)
 {
@@ -22,6 +22,10 @@ New_list->next->prev = New_list->prev;
 New_list->next = New_list->prev;
 New_list->prev = New_list->prev->prev;
 New_list->next->prev = New_list;
+if (New_list->prev == NULL)
+*list = New_list;
+else
+New_list->prev->next = New_list;
 print_list(*list);
 }
 New_list = New_list->next;
